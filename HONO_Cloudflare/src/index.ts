2987870bff1da0d943813d81f4c1966717fc3854
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-02-17 16:24:34 +0530

    Add authMiddleware and update route handlers

--- HONO_Cloudflare/src/index.ts
+++ HONO_Cloudflare/src/index.ts
@@ -1,19 +1,26 @@
-import { Hono } from "hono";
+import { Hono, Next } from 'hono'
+import { Context } from 'hono/jsx';
 
-const app = new Hono();
+const app = new Hono()
 
-app.post("/", async (c) => {
-  const body = await c.req.json();
+async function authMiddleware(c: any, next: any) {
+  
+
+  if (c.req.header("Authorization")) {
+
+    await next()
+  } else {
+    return c.text("You don't have access");
+  }
+}
+
+app.get('/',authMiddleware, async (c) => {
+  const body = await c.req.parseBody()
   console.log(body);
   console.log(c.req.header("Authorization"));
-  console.log(c.req.header("param"));
-  return c.text("Hello Hono!");
-});
+  console.log(c.req.query("param"));
 
-app.post("/user", (c) => {
-  return c.json({
-    name: "hono",
-  });
-});
+  return c.json({msg: "as"})
+})
 
-export default app;
+export default app
