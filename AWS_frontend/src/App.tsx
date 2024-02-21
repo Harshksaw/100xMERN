f728a0d8055357045f32511eb7a7b653c56d46a0
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-02-21 16:02:06 +0000

    AWS s3 |Deploy

---
+++ AWS_frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>AWS Frotend Deployment</h1>
+      <main className="container mx-auto px-4 py-8">
+        <p>This is the main content area.</p>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Click Me
+        </button>
+      </main>
+    </>
+  );
+}
+
+export default App;
