0f3fd96a79ca6b09bf9709dfd29fc9e5009ea041
Harsh kumar <mister.harshkumar@gmail.com>
2024-04-15 17:00:46 +0530

    Add Book resolver in GraphQL resolvers.js

--- frontend/graphql/resolvers.js
+++ frontend/graphql/resolvers.js
@@ -1,21 +1,54 @@
+
+const data = {
+  authors: [
+    {
+      id: 1,
+      name: "Chirag Goel",
+    },
+    {
+      id: 1,
+      name: "CHarhsh Goel",
+    },
+  ],
+  books: [
+    {
+      id: 13,
+      title: "Hars  kumar",
+      publishedYear: 2024,
+    },
+    {
+      id: 13,
+      title: "Hardssd  kumar",
+      publishedYear: 2024,
+    },
+    {
+      id: 12,
+      title: "Harsddssfs  kumar",
+      publishedYear: 2024,
+    },
+  ],
+};
+
+
 export const resolvers = {
+
+
+  Book: {
+    author: (parent,args , context , info) => {
+      return data.authors.find((author) => author.id === parent.id);
+
+    }
+  }
   Query: {
     authors: () => {
-      return [
-        {
-          id: 1,
-          name: "Chirag Goel",
-        },
-      ];
+      return        data.authors
+
+
     },
     books: () => {
-      return [
-        {
-          id: 1,
-          title: "Hars  kumar",
-          publishedYear: 2024,
-        },
-      ];
+      return data.books;
+
+
     },
   },
 };
