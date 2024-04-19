d2c3208eabee8ac139f27f6debeff075838bc39e
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-04-19 15:27:23 +0000

    OpenAPI | hono OpenAPI | clientGenerator

---
+++ cohort2/week-20/OpenAPIHhono/generated/services/DefaultService.ts
@@ -0,0 +1,26 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { User } from '../models/User';
+import type { CancelablePromise } from '../core/CancelablePromise';
+import { OpenAPI } from '../core/OpenAPI';
+import { request as __request } from '../core/request';
+export class DefaultService {
+    /**
+     * @param id
+     * @returns User Retrieve the user
+     * @throws ApiError
+     */
+    public static getUsers(
+        id: string,
+    ): CancelablePromise<User> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/users/{id}',
+            path: {
+                'id': id,
+            },
+        });
+    }
+}
